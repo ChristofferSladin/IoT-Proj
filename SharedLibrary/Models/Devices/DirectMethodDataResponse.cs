0392a39d728061ee0ebe735ef175feaed18c2a8b
Christoffer Sladin <christoffer.sladin@hotmail.com>
2023-09-13 13:31:54 +0200

    Add project files.

---
+++ SharedLibrary/Models/Devices/DirectMethodDataResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Models.Devices
+{
+    public class DirectMethodDataResponse
+    {
+        public string? Message { get; set; }
+        public string? Payload { get; set; }
+    }
+}
---
+++ SharedLibrary/Services/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Services
+{
+    public class NetworkManager
+    {
+        public virtual async Task<string> CheckConnectivityAsync(string ipAdress = "8.8.8.8")
+        {
+            var isConnected = await SendPingAsync(ipAdress);
+            return isConnected ? "Connected" : "Disconnected";
+        }
+
+        private async Task<bool> SendPingAsync(string ipAdress)
+        {
+            try
+            {
+                using var ping = new Ping();
+
+                var reply = await ping.SendPingAsync(ipAdress, 1000, new byte[32], new());
+                return reply.Status == IPStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
