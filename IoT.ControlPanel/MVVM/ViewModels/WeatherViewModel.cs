fb2cd1765860eeec2127877797959c0a202dede1
Christoffer Sladin <christoffer.sladin@hotmail.com>
2023-10-04 12:16:11 +0200

    added timer for api call

--- IoT.ControlPanel/MVVM/ViewModels/WeatherViewModel.cs
+++ IoT.ControlPanel/MVVM/ViewModels/WeatherViewModel.cs
@@ -8,6 +8,18 @@ namespace IoT.ControlPanel.MVVM.ViewModels
 {
     public partial class WeatherViewModel : ObservableObject
     {
+        private Timer _timer;
+
+        public WeatherViewModel()
+        {
+            _timer = new Timer(AsyncCallback, null, 0, 900000);
+        }
+
+        private async void AsyncCallback(object state)
+        {
+            await GetWeatherAsync();
+        }
+
         private string _location;
         private string _temperature;
         private string _weatherDescription;
@@ -56,6 +68,44 @@ namespace IoT.ControlPanel.MVVM.ViewModels
             return Math.Round(celsius);
         }
 
+        public string GetWeatherIcon(string iconCode)
+        {
+            switch (iconCode)
+            {
+                case "01d":
+                    return "\uf185";  // fas fa-sun
+                case "01n":
+                    return "\uf2c9";  // fas fa-moon (assuming moon for clear sky at night)
+                case "02d":
+                case "02n":
+                    return "\uf6c4";  // fas fa-cloud-sun
+                case "03d":
+                case "03n":
+                    return "\uf6c4";  // fas fa-cloud-sun (assuming partly cloudy for scattered clouds)
+                case "04d":
+                case "04n":
+                    return "\uf0c2";  // fas fa-cloud
+                case "09d":
+                case "09n":
+                    return "\uf73d";  // fas fa-cloud-rain
+                case "10d":
+                case "10n":
+                    return "\uf740";  // fas fa-cloud-showers-heavy
+                case "11d":
+                case "11n":
+                    return "\uf0e7";  // fas fa-bolt
+                case "13d":
+                case "13n":
+                    return "\uf2dc";  // fas fa-snowflake
+                case "50d":
+                case "50n":
+                    return "\uf75f";  // fas fa-smog (assuming smog for mist)
+                default:
+                    return "";  // Return an empty string or a default icon code if iconCode is not recognized
+            }
+        }
+
+
         public async Task GetWeatherAsync()
         {
             var client = new HttpClient();
@@ -78,7 +128,7 @@ namespace IoT.ControlPanel.MVVM.ViewModels
 
                 // Populate individual properties
                 Location = data.Name;
-                Icon = data.Weather[0].Icon;
+                Icon = GetWeatherIcon(data.Weather[0].Icon);
                 Temperature = $"{ConvertFahrenheitToCelsius(data.Main.Temp)}°C";
                 WeatherDescription = $"{data.Weather[0].Main} ({data.Weather[0].Description})";
                 WindSpeed = data.Wind.Speed;
